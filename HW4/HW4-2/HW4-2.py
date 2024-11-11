7eb15b502ff1d187967c5c3cafd6515e911f36f3
yojun313 <moonyojun@gmail.com>
2024-11-11 15:47:37 +0900

    update

--- HW4/HW4-2/HW4-2.py
+++ HW4/HW4-2/HW4-2.py
@@ -5,11 +5,12 @@ import pandas as pd
 train_data = pd.read_csv('train.csv')
 train_data = train_data.dropna()  # 결측치 제거
 
-# Drop non-numeric and target columns, and rows with NaN values in 'MIN' column for regression
+# 비숫자 열과 목표 열 제거, 회귀분석을 위해 'MIN' 열에 결측치가 있는 행 제거
 data_for_regression = train_data.drop(columns=['SEASON_ID', 'TEAM_ID', 'position', 'FG_PCT', 'FG3_PCT', 'FT_PCT', 'FG3M', 'FG3A']).dropna(subset=['MIN'])
 X = data_for_regression.drop(columns=['MIN'])
 y = data_for_regression['MIN']
 
+# KNN 회귀 모델 생성 및 학습
 KNNObj = KNeighborsRegressor()
 KNNObj.fit(X, y)
 
@@ -20,7 +21,7 @@ real_data = real_data.drop(columns=['SEASON_ID', 'TEAM_ID', 'ID', 'FG_PCT', 'FG3
 # real_data에 대한 예측
 real_pred = KNNObj.predict(real_data)
 
-# real_pred_labels를 position 열로, ID 열을 1부터 시작하는 데이터프레임 생성
+# real_pred 값을 position 열로 사용하고 ID 열은 1부터 시작하는 데이터프레임 생성
 result_df = pd.DataFrame({
     'ID': range(1, len(real_pred) + 1),
     'MIN': real_pred
@@ -28,7 +29,3 @@ result_df = pd.DataFrame({
 
 # 결과를 CSV 파일로 저장
 result_df.to_csv('result.csv', index=False)
-
-
-
-
